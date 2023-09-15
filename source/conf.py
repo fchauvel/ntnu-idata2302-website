69373db91acf752396d5bbbbaeef0070c62b17e4
Franck Chauvel <fchauvel@Francks-MacBook-Air.local>
2023-09-15 17:43:18 +0200

    release: v0.3.18

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, NTNU'
 author = 'NTNU'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.17'
+release = '0.3.18'
 
 # -- General configuration ---------------------------------------------------
 
