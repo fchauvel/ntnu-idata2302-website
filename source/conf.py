36186197e3794b5d49db076ee9c97675a4983a7d
Franck Chauvel <fchauvel@dhcp-10-24-89-75.wlan.ntnu.no>
2023-09-20 08:15:07 +0200

    release: v0.3.21

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, NTNU'
 author = 'NTNU'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.20'
+release = '0.3.21'
 
 # -- General configuration ---------------------------------------------------
 
