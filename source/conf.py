759b52ad5f690214a6d89ab333267302323f651b
Franck Chauvel <franck.chauvel@dipai.no>
2024-08-25 15:50:12 +0200

    release: v0.4.4

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, NTNU'
 author = 'NTNU'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.3'
+release = '0.4.4'
 
 # -- General configuration ---------------------------------------------------
 
