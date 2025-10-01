908efc466e0fb3f8fbc5e628dbf64ea2ca7d87da
Franck Chauvel <franck.chauvel@gmail.com>
2025-10-01 10:02:29 +0200

    release: v0.5.10

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, NTNU"
 author = "NTNU"
 
 # The full version, including alpha/beta/rc tags
-release = "0.5.9"
+release = "0.5.10"
 
 # -- General configuration ---------------------------------------------------
 
